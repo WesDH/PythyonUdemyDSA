ead46d75b20cd7b08cdb2df3910c2bc81570d5d4
WesDH <wesleydh85@gmail.com>
2023-04-05 08:51:20 -0500

    initial stack self implementation

---
+++ stacks_queues_decks/stack_self_implement.py
@@ -0,0 +1,19 @@
+class Stack(object):
+    def __init__(self):
+        self.items = []
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        if self.items == []:
+            print("Cannot pop from stack, stack is empty.")
+            return
+        return self.items.pop()
+    def is_empty(self):
+        return self.items == []
+    def size(self):
+        return len(self.items)
+    def peek(self):
+        if self.items == []:
+            print("Cannot peek stack, stack is empty.")
+            return
+        return self.items[-1]
