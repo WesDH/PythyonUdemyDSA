81a711385eabd4644450a06d0bdf7b0a63ba50a5
WesDH <wesleydh85@gmail.com>
2023-04-05 12:51:02 -0500

    Re add deque self implemt

---
+++ stacks_queues_decks/deque_si.py
@@ -0,0 +1,33 @@
+class Deque(object):
+    def __init__(self):
+        self.items = []
+    def add_front(self, item):
+        self.items.append(item)
+    def add_rear(self, item):
+        self.items.insert(0, item)
+    def rm_front(self):
+        if self.items == []:
+            print("cannot rm front, Deque empty.")
+            return
+        return self.items.pop()
+    def rm_rear(self):
+        if self.items == []:
+            print("cannot rm rear, Deque empty.")
+            return
+        return self.items.pop(0)
+    def size(self):
+        return len(self.items)
+    def is_empty(self):
+        return self.items == []
+
+
+if __name__ == "__main__":
+    d = Deque()
+    print("Add Hello to front")
+    d.add_front("Hello")
+    print("Add World to rear")
+    d.add_rear("World")
+    print("d.size(): ", d.size())
+    print("rm front and rm rear: ", d.rm_front(), d.rm_rear())
+    print("d.size(): ", d.size())
+    print("d.is_empty(): ", d.is_empty())
