7772e46b32003040a7af1fb03592e6ba53623f04
WesDH <wesleydh85@gmail.com>
2023-04-05 05:08:14 -0500

    finished Queue class

--- stacks_queues_decks/queue.py
+++ stacks_queues_decks/queue.py
@@ -1,9 +1,9 @@
 class Queue(object):
     def __init__(self):
         self.items = []
-    def add(self, item):
+    def enqueue(self, item):
         self.items.append(item)
-    def pop(self):
+    def dequeue(self):
         if self.items == []:
             print("Queue empty, nothing to remove!")
             return
@@ -12,9 +12,21 @@ class Queue(object):
         return self.items == []
     def peek(self):
         if self.items == []:
-            print("Queue emptyiii, nothing to peek!")
-        return self.items[len(self.items) - 1]]
+            print("Queue empty, nothing to peek!")
+            return
+        return self.items[0]
     def size(self):
-        return len(self.items
+        return len(self.items)
 
-                )
+
+if __name__ == "__main__":
+    q = Queue()
+    print("is_empty():", q.is_empty())
+    print("size(): ", q.size())
+    print("enqueue 1: ", q.enqueue(1))
+    print("enqueue: 2", q.enqueue(2))
+    print("dequeue: ", q.dequeue())
+    print("peek: ", q.peek())
+    print("dequeue: ", q.dequeue())
+    print("dequeue: ", q.dequeue())
+    print("peek: ", q.peek())
